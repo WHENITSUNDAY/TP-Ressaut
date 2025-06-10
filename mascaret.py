6c5c5f73c8bfddd00b8bfd7685ee0f1b0be3a82b
WHENITSUNDAY <julien.leger@bordeaux-inp.fr>
2025-06-10 16:09:34 +0200

    Ajustement au niveau des formes des obstacles et de la visualisation de la simulation

--- mascaret.py
+++ mascaret.py
@@ -9,15 +9,17 @@ from matplotlib.patches import Polygon
 pour la rivière, avec une pente quadratique. Cela permettra d'accentuer le ressaut etde mettre en avant le phénomène ondulatoire du Mascaret. 
 Le schéma se base en partie sur cela d'article "Numerical Simulation of Tidal Bore Bono at Kampar River (JAFM, A. C. Bayu et al)"""
 
+show_velocity = False      #True si on veut afficher le profil de la vitesse pendant la simulation
 show_river = True          #True si on veut afficher la forme de la rivière en largeur
 show_bathymetry = True     #True si on veut afficher la bathymétrie (profil de la profondeur) de la rivière
-bathymetry_shape = 4 #1 pour constant, 2 pour linéaire, 3 pour quadratique (le plus réaliste), 4 pour quadratique avec une bosse (voire plusieurs)
-L = 8e4             #Longueur caractéristique de l'écoulement
-h_estuary = 6       #Profondeur en aval (gauche)
+bathymetry_shape = 3       #1 pour constant, 2 pour linéaire, 3 pour quadratique (le plus réaliste)
+obstacle_shape = 3         #1 pour un créneau, 2 pour une rampe, 3 pour une bosse (gausienne)
+
+L = 80000             #Longueur caractéristique de l'écoulement
+h_estuary = 8       #Profondeur en aval (gauche)
 h_river = 3         #Profondeur en amont (droite) -> Utilisée seulement si profondeur non constante
 b_river = 200       #Larguer de la rivière en amont
-b_estuary = 12000   #Largeur de la rivière en aval (estuaire)
-Q_river = 800.0     #Débit de la rivière en amont (pas utilisé pour l'instant)
+b_estuary = 5000   #Largeur de la rivière en aval (estuaire)
 
 tide_amplitude = 2                  #Amplitude de la marée
 tide_period = 12 * 3600 + 25 * 60   #Période de l'onde de marée -> 12h25m
@@ -27,9 +29,8 @@ n_manning = 0.001       #Coefficient de frottement de manning
 nx = 1024
 dx = L / nx
 x = np.linspace(0, L, nx)
-
 g = 9.81
-cfl = 0.01
+cfl = 0.1
 dt = cfl * dx / (np.sqrt(g * h_estuary + tide_amplitude))
 c = np.sqrt(g * h_estuary)   # vitesse d'onde (approximation mascaret)
 tmax = 1.2 * L / c  
@@ -42,16 +43,34 @@ if bathymetry_shape == 1: #Constant
     zb = np.full(nx, -h_static)
 
 elif bathymetry_shape == 2: #Linéaire (pour les élèves)
-
-    zb = 0 #à modifier
+    zb = 0
 
 elif bathymetry_shape == 3: #Quadratique
     zb = -h_river + (h_river - h_estuary) * (1 - x / L)**2
 
-elif bathymetry_shape == 4:
-    zb = -h_river + (h_river - h_estuary) * (1 - x / L)**2
-    bump_height = 2
-    bump_width = 5e3
+    
+if obstacle_shape == 1:
+    slot_height = 3
+    slot_width = 5000
+    slot_center = L/2
+    start_idx = int((slot_center - slot_width/2) / L * nx)
+    end_idx = int((slot_center + slot_width/2) / L * nx)
+    zb[start_idx:end_idx] += slot_height  
+
+elif obstacle_shape == 2:
+    ramp_width = 10000
+    ramp_x0 = L/2
+    ramp_height = 2
+    start_idx = int((ramp_x0) / L * nx)
+    end_idx = int((ramp_x0 + ramp_width) / L * nx)
+
+    for i in range(start_idx, end_idx):
+
+        zb[i] += ramp_height * (i - start_idx) / (end_idx - start_idx)
+
+elif obstacle_shape == 3:
+    bump_height = 1
+    bump_width = 20000
     bump_center = L/2
     zb = zb + bump_height * np.exp(-(x - bump_center)**2 / (2 * (bump_width / 4)**2))
 
@@ -62,9 +81,8 @@ if show_river :
     fig, ax = plt.subplots(figsize=(12, 4))
     river_left = -b / 2
     river_right = b / 2
-    river_coords = np.vstack([np.column_stack((x / 1000, river_left)), np.column_stack((x[::-1] / 1000, river_right[::-1]))
-    ])
-    river_polygon = Polygon(river_coords, closed=True, color='royalblue', alpha=0.8, linewidth=0)
+    river_coords = np.vstack([np.column_stack((x / 1000, river_left)), np.column_stack((x[::-1] / 1000, river_right[::-1]))])
+    river_polygon = Polygon(river_coords, closed=True, color='dodgerblue', alpha=0.8, linewidth=0)
     ax.add_patch(river_polygon)
     land_width = 1.2 * np.max(b)
     ax.set_ylim(-land_width, land_width)
@@ -80,12 +98,12 @@ if show_river :
 
 if show_bathymetry :
     fig, ax = plt.subplots(figsize=(12, 4))
-    ax.set_ylim(-h_estuary, 0)
+    ax.set_ylim(-h_estuary - 2, 0)
     ax.set_xlim(0, L/1000)
     ax.plot(x / 1000, np.zeros_like(x), color='deepskyblue', lw=2, label="Surface de l'eau (z=0)")
     ax.plot(x / 1000, zb, color='saddlebrown', lw=2, label="Lit de la rivière")
-    ax.fill_between(x / 1000, zb, 0, where=zb < 0, color='deepskyblue', alpha=0.7)
-    ax.fill_between(x / 1000, zb, np.min(zb)-1, color='saddlebrown', alpha=0.6)
+    ax.fill_between(x / 1000, zb, 0, where=zb < 0, color='dodgerblue', alpha=0.7)
+    ax.fill_between(x / 1000, zb, np.min(zb)-2, color='saddlebrown', alpha=0.6)
     ax.set_xlabel("Distance (km)")
     ax.set_ylabel("Altitude (m)")
     ax.set_title("Bathymétrie de la rivière (surface et profondeur)")
@@ -118,7 +136,6 @@ def compute_tidal_bore(nframes, nx, dx, dt, g, b, zb, h_estuary, tide_amplitude,
         c1 = np.sqrt(g * h[1])
         u[0] = u[1] + 2 * (c_in - c1)
         
-        
         for j in range(1, nx-1):
             h_phalf = 0.5 * (h[j] + h[j+1])
             h_mhalf = 0.5 * (h[j] + h[j-1])
@@ -144,7 +161,7 @@ start = time.time()
 nframes = int((tmax)/dt)
 
 all_eta, all_u, all_time = compute_tidal_bore(nframes, nx, dx, dt, g, b, zb, h_estuary, tide_amplitude, tide_period, n_manning)
-print (f"Temps de calcul : {time.time() - start:.2f} secondes")
+print (f"Temps de calcul des solutions: {time.time() - start:.2f} secondes")
 
 all_u = all_u[:, :-1]
 
@@ -163,17 +180,29 @@ ax.set_xlabel("Distance de l'estuaire vers Podensac (km)", fontsize=12, labelpad
 ax.set_ylabel("Élévation (m)", fontsize=12, labelpad=10)
 ax.set_title(f"Propagation du mascaret - Temps: 0 min", fontsize=14, pad=20)
 ax.grid(True, alpha=0.2)
-ax.text(0.02, 0.85, "Estuaire de la Gironde\n (marée montante)", transform=ax.transAxes, fontsize=12, fontweight='bold', color='navy')
-ax.text(0.85, 0.85, "Podensac", transform=ax.transAxes, fontsize=12, fontweight='bold', color='navy')
-ax.axhline(y=-h_estuary, color='k', linestyle='--', linewidth=1, label='Fond marin')
-ax.fill_between(x/1000, np.min(zb)-2, zb, color='saddlebrown', alpha=0.7, label='Lit de la rivière')
-water_surface, = ax.plot(x/1000, all_eta[0, :], color='deepskyblue', lw=2, label="Surface de l'eau")
+ax.text(0.02, 0.55, "Estuaire de la Gironde\n (marée montante)", transform=ax.transAxes, fontsize=12, fontweight='bold', color='navy')
+ax.text(0.85, 0.55, "Podensac", transform=ax.transAxes, fontsize=12, fontweight='bold', color='navy')
+ax.plot(x / 1000, zb, color='saddlebrown', lw=2, label="Lit de la rivière")
+ax.fill_between(x/1000, np.min(zb)-2, zb, color='saddlebrown', alpha=0.6)
+
+water_surface, = ax.plot(x/1000, all_eta[0, :], color='skyblue', lw=2, label="Surface de l'eau")
+
+if show_velocity:
+    stride = max(1, nx // 50)
+    x_quiver = x[::stride] / 1000
+    eta_mid = (all_eta[0, ::stride] + zb[::stride]) / 2
+    quiver = ax.quiver(x_quiver, eta_mid, all_u[0, ::stride], np.zeros_like(x_quiver), scale=100, color='red', width=0.003)
 
 def update(frame):
     water_polygon.set_xy(np.vstack([np.concatenate([x/1000, x[::-1]/1000]), np.concatenate([all_eta[frame, :], zb[::-1]])]).T)
     water_surface.set_ydata(all_eta[frame, :])
-    time = int(all_time[frame] // 60)
-    ax.set_title(f"Propagation du mascaret - Temps: {time} min", fontsize=14, pad=20)
+    
+    if show_velocity:
+        eta_mid = (all_eta[frame, ::stride] + zb[::stride]) / 2
+        quiver.set_offsets(np.column_stack([x_quiver, eta_mid]))
+        quiver.set_UVC(all_u[frame, ::stride], np.zeros_like(x_quiver))
+    ax.set_title(f"Propagation du mascaret - Temps: {int(all_time[frame] // 60)} min", fontsize=14)
+
     return water_polygon
 
 water_polygon = Polygon(np.vstack([np.concatenate([x/1000, x[::-1]/1000]),np.concatenate([all_eta[0, :], zb[::-1]])]).T,color='dodgerblue', alpha=0.7, label='Eau')
